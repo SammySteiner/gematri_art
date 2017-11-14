0ba5e353196b47dd587fc8e2f592ecebd0c72819
SammySteiner <sammysteiner@gmail.com>
2017-11-13 23:03:59 -0500

    experimenting with line angles and continued to stub out code by breaking down requirements into helper functions

--- index.js
+++ index.js
@@ -2,6 +2,55 @@ function hello() {
   return alert("hello")
 }
 
+var printButton = document.getElementById('submit')
+
+printButton.addEventListener('click', function(event){
+  event.preventDefault()
+  var word = getWord()
+  wordToArt(word)
+  clearInput()
+})
+
+function getWord() {
+  return document.getElementById('word').value
+}
+
+function clearInput() {
+  document.getElementById('word').value = ""
+}
+
+function wordToArt(word) {
+  var coordinatesForAllLetters = wordToCoords(word)
+  var linesForAllLetters = coordsToLines(coordinatesForAllLetters)
+  document.getElementById('art-svg').innerHTML = linesForAllLetters
+}
+
+function wordToCoords(word) {
+
+}
+
+function letterToAngle(letter) {
+  return (letter.charCodeAt(0) -96) * 6.88
+}
+
+function angleToCoords(angle, xMax, yMax) {
+
+}
+
+function findMidpoint(xMax, yMax) {
+
+}
+
+function coordsToLines() {
+
+}
+
+
+
+
+
+
+
 // when a user hits submit:
 // 1. prevent refresh
 // 2. grab the input
